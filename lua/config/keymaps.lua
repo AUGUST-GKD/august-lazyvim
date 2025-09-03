557d2385a5951cd1a1c464fb3e2a1413d86265fe
AUGUST-GKD <54925899+AUGUST-GKD@users.noreply.github.com>
2025-09-03 16:07:21 +0800

    1st

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,46 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+--  insert mode ----
+keymap.set("i", "jk", "<ESC>")
+
+-- visual mode ----
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+--  normal ----
+--  window
+-- keymap.set("n", "<leader>sv", "<C-w>v")
+-- keymap.set("n", "<leader>sh", "<C-w>s")
+
+-- cancel highlight
+-- keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- nvim tree
+-- keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+-- switch buffer
+
+keymap.set("n", "<C-]>", "<cmd>bnext<CR>")
+keymap.set("n", "<M-Right>", "<cmd>bnext<CR>")
+keymap.set("n", "<C-[>", "<cmd>bprevious<CR>")
+keymap.set("n", "<M-Left>", "<cmd>bprevious<CR>")
+-- keymap.set("n", "<leader>c", ":BufferLinePickClose <CR>")
+-- keymap.set("n", "<leader>;'", ":BufferLineCloseOthers <CR>")
+-- keymap.set("n", "<C-<>", ":BufferLineCloseLeft <CR>")
+-- keymap.set("n", "<C->>", ":BufferLineCloseRight <CR>")
+
+keymap.set("n", ";", ":")
+
+keymap.set("n", "L", "$")
+keymap.set("v", "L", "$")
+keymap.set("n", "H", "^")
+keymap.set("v", "H", "^")
+
+keymap.set("n", "<leader>nv", "<cmd>Navbuddy<CR>")
+keymap.set("t", "<ESC>", "<C-\\><C-n>")
+keymap.set("t", "[", "<Up>")
+keymap.set("t", "]", "<Down>")
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+-- lua/plugins/disabled.lua
+return {
+  -- { "akinsho/bufferline.nvim", enabled = false },
+  -- 如果你想禁用其他插件，在这里添加更多行
+  -- { "插件作者/插件仓库名", enabled = false },
+}
